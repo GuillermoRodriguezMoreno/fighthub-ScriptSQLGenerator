95be015fa888839ae9bef1ea42b4b50245cb9889
guille <guille@ejemplo.es>
2024-05-29 17:26:22 +0200

    terminado??

--- insert_data.py
+++ insert_data.py
@@ -11,6 +11,7 @@ fake.add_provider(provider.AndaluciaCityProvider)
 fake.add_provider(provider.GymNameProvider)
 fake.add_provider(provider.FightEventProvider)
 
+
 # FIGHTER
 def generate_fighter_insert_statement(number_of_inserts):
     insert_statement = "-- FIGHTER"
@@ -37,54 +38,19 @@ def generate_fighter_insert_statement(number_of_inserts):
         wins_in_a_row = random.randint(0, wins)
         category_id = random.randint(1, 4)
         insert_statement += f"""
-INSERT INTO fighter (   birth_date, 
-                        email, 
-                        password, 
-                        register_date, 
-                        user_name, 
-                        profile_photo_id,
-                        active, 
-                        biography, 
-                        draws, 
-                        gender, 
-                        height, 
-                        kos, 
-                        latitude, 
-                        longitude,
-                        losses, 
-                        name, 
-                        number_of_fights, 
-                        weight, 
-                        wins, 
-                        wins_in_a_row, 
-                        category_id)
-                        
-    VALUES ('{birth_date}', 
-            '{email}', 
-            '{password}', 
-            '{register_date}', 
-            '{user_name}', 
-            {f"'{profile_photo_id}'" if profile_photo_id else 'NULL'},
-            {active}, 
-            '{biography}', 
-            {draws}, 
-            '{gender}', 
-            {height:.2f}, 
-            {kos}, 
-            {latitude:.6f}, 
-            {longitude:.6f},
-            {losses}, 
-            '{name}', 
-            {number_of_fights}, 
-            {weight:.2f}, 
-            {wins}, 
-            {wins_in_a_row}, 
-            {category_id});
+INSERT INTO fighter (   birth_date, email, password, register_date, user_name, profile_photo_id, active, biography, 
+                        draws, gender, height, kos, latitude, longitude, losses, name, number_of_fights, weight, 
+                        wins, wins_in_a_row, category_id)
+    VALUES ('{birth_date}', '{email}', '{password}', '{register_date}', '{user_name}', 
+            {f"'{profile_photo_id}'" if profile_photo_id else 'NULL'}, {active}, '{biography}', 
+            {draws}, '{gender}', {height:.2f}, {kos}, {latitude:.6f}, {longitude:.6f}, {losses}, 
+            '{name}', {number_of_fights}, {weight:.2f}, {wins}, {wins_in_a_row}, {category_id});
 """
         insert_statement = insert_statement.strip()
         insert_statement += "\n"
 
     return insert_statement
+
 
 # FIGHTER_STYLE
 def generate_fighter_style_insert_statement(total_fighters, total_styles):
@@ -92,15 +58,14 @@ def generate_fighter_style_insert_statement(total_fighters, total_styles):
     for fighter_id in range(1, total_fighters + 1):
         for style_id in range(random.randint(1, total_styles), total_styles + 1):
             insert_statement += f"""
-INSERT INTO fighter_style ( fighter_id, 
-                            style_id) 
-    VALUES ({fighter_id}, 
-            {style_id});
+INSERT INTO fighter_style ( fighter_id, style_id) 
+    VALUES ({fighter_id}, {style_id});
 """
             insert_statement = insert_statement.strip()
             insert_statement += "\n"
 
     return insert_statement
+
 
 # CLUB
 def generate_club_insert_statement(number_of_inserts, total_fighters):
@@ -119,36 +84,16 @@ def generate_club_insert_statement(number_of_inserts, total_fighters):
         deleted = False
         president_id = fake.unique.random_int(min=1, max=total_fighters)
         insert_statement += f"""
-INSERT INTO club (  name, 
-                    city, 
-                    country, 
-                    postal_code, 
-                    state, 
-                    street, 
-                    phone, 
-                    email, 
-                    register_date,
-                    description, 
-                    deleted, 
-                    president_id)
-                    
-    VALUES ('{name}', 
-            '{city}', 
-            '{country}', 
-            '{postal_code}', 
-            '{state}', 
-            '{street}', 
-            '{phone}', 
-            '{email}',
-            '{register_date}', 
-            '{description}', 
-            {deleted}, 
-            {president_id});
+INSERT INTO club (  name, city, country, postal_code, state, street, phone, email, register_date, description, 
+                    deleted, president_id)
+    VALUES ('{name}', '{city}', '{country}', '{postal_code}', '{state}', '{street}', '{phone}', '{email}',
+            '{register_date}', '{description}', {deleted}, {president_id});
 """
         insert_statement = insert_statement.strip()
         insert_statement += "\n"
 
     return insert_statement
+
 
 # FIGHTER_ROLE
 def generate_fighter_role_insert_statement(total_fighters):
@@ -156,16 +101,14 @@ def generate_fighter_role_insert_statement(total_fighters):
     for fighter_id in range(1, total_fighters + 1):
         role_id = random.randint(1, 3)
         insert_statement += f"""
-INSERT INTO fighter_role (  fighter_id, 
-                            role_id) 
-                            
-    VALUES ({fighter_id}, 
-            {role_id});
+INSERT INTO fighter_role (  fighter_id, role_id) 
+    VALUES ({fighter_id}, {role_id});
 """
         insert_statement = insert_statement.strip()
         insert_statement += "\n"
 
     return insert_statement
+
 
 # ASSOCIATE FIGHTERS WITH CLUB
 def associate_fighters_with_club(total_fighters, total_clubs):
@@ -181,6 +124,64 @@ UPDATE fighter SET club_id = {club_id} WHERE id = {fighter_id};
     return insert_statement
 
 
+# PROFILE PHOTO OF FIGHTERS
+def generate_fighter_profile_photo_insert_statement(total_fighters):
+    insert_statement = "-- FIGHTER PROFILE PHOTO"
+    URL = "./assets/img/fighter/fighter"
+    for fighter_id in range(1, total_fighters + 1):
+        entity_type = 'FIGHTER'
+        upload_date = fake.date_time_this_year(before_now=True).strftime('%Y-%m-%d %H:%M')
+        url = URL + str(random.randint(1, 28)) + ".jpg"
+        ##### INSERT STATEMENT ####
+        insert_statement += f"""
+INSERT INTO photo ( entity_type, upload_date, url, fighter_id)
+    VALUES ('{entity_type}', '{upload_date}', '{url}', {fighter_id});
+"""
+        insert_statement = insert_statement.strip()
+        insert_statement += "\n"
+
+        # #################################### #
+        # ASSOCIATE PROFILE PHOTO WITH FIGHTER #
+        # #################################### #
+        #### UPDATE STATEMENT ####
+        insert_statement += f"""
+UPDATE fighter SET profile_photo_id = {fighter_id} WHERE id = {fighter_id};
+"""
+        insert_statement = insert_statement.strip()
+        insert_statement += "\n"
+
+    return insert_statement
+
+
+# PROFILE PHOTO OF CLUBS
+def generate_club_photo_insert_statement(total_clubs):
+    insert_statement = "-- CLUB PROFILE PHOTO"
+    URL = "./assets/img/club/club"
+    for club_id in range(1, total_clubs + 1):
+        entity_type = 'CLUB'
+        upload_date = fake.date_time_this_year(before_now=True).strftime('%Y-%m-%d %H:%M')
+        url = URL + str(random.randint(1, 12)) + ".jpg"
+        ##### INSERT STATEMENT ####
+        insert_statement += f"""
+INSERT INTO photo ( entity_type, upload_date, url, club_id)
+    VALUES ('{entity_type}', '{upload_date}', '{url}', {club_id});
+"""
+        insert_statement = insert_statement.strip()
+        insert_statement += "\n"
+
+        # ################################# #
+        # ASSOCIATE PROFILE PHOTO WITH CLUB #
+        # ################################# #
+        #### UPDATE STATEMENT ####
+        insert_statement += f"""
+UPDATE club SET profile_photo_id = {club_id} WHERE id = {club_id};
+"""
+        insert_statement = insert_statement.strip()
+        insert_statement += "\n"
+
+    return insert_statement
+
+
 # FOLLOW
 def generate_follow_insert_statement(total_fighters):
     insert_statement = "-- FOLLOW"
@@ -189,13 +190,8 @@ def generate_follow_insert_statement(total_fighters):
         followed_id = random.randint(1, total_fighters)
         if follower_id != followed_id:
             insert_statement += f"""
-INSERT INTO follow (follow_date, 
-                    follower_fighter_id, 
-                    followed_fighter_id)
-                     
-    VALUES ('{follow_date}', 
-            {follower_id}, 
-            {followed_id});
+INSERT INTO follow (follow_date, follower_fighter_id, followed_fighter_id)
+    VALUES ('{follow_date}', {follower_id}, {followed_id});
 """
             insert_statement = insert_statement.strip()
             insert_statement += "\n"
@@ -214,17 +210,8 @@ def generate_fighter_follow_request_insert_statement(total_fighters):
         for receiver_id in range(random.randint(1, total_fighters), total_fighters + 1):
             if sender_id != receiver_id:
                 insert_statement += f"""
-INSERT INTO fighter_follow_request (request_date, 
-                                    response_date, 
-                                    status,
-                                    receiver_fighter_id, 
-                                    sender_fighter_id)
-                                     
-    VALUES ('{request_date}', 
-            '{response_date}', 
-            '{status}', 
-            {receiver_id}, 
-            {sender_id});
+INSERT INTO fighter_follow_request (request_date, response_date, status, receiver_fighter_id, sender_fighter_id)
+    VALUES ('{request_date}', '{response_date}', '{status}', {receiver_id}, {sender_id});
 """
                 insert_statement = insert_statement.strip()
                 insert_statement += "\n"
@@ -250,28 +237,10 @@ def generate_event_insert_statement(number_of_inserts, total_clubs):
         club_id = fake.random_int(min=1, max=total_clubs)
 
         insert_statement += f"""
-INSERT INTO event ( city, 
-                    country, 
-                    postal_code, 
-                    state, street, 
-                    description, 
-                    end_date, 
-                    name, 
-                    open_date, 
-                    start_date, 
-                    club_id)
-                    
-    VALUES (    '{city}', 
-                '{country}', 
-                '{postal_code}', 
-                '{state}', 
-                '{street}', 
-                '{description}',
-                '{end_date}', 
-                '{name}', '
-                {open_date}',
-                '{start_date}', 
-                {club_id});
+INSERT INTO event ( city, country, postal_code, state, street, description, end_date, name, open_date, 
+                    start_date, club_id)                
+    VALUES (    '{city}', '{country}', '{postal_code}', '{state}', '{street}', '{description}', '{end_date}', 
+                '{name}', '{open_date}', '{start_date}', {club_id});
 """
         insert_statement = insert_statement.strip()
         insert_statement += "\n"
@@ -284,22 +253,14 @@ def generate_club_membership_request_insert_statement(total_fighters, total_club
     insert_statement = "-- CLUB_MEMBERSHIP_REQUEST"
     for fighter_id in range(1, total_fighters + 1):
         request_date = fake.date_time_this_year(before_now=True)
-        response_date = fake.date_time_between(start_date=request_date, end_date=datetime.now().date()).strftime('%Y-%m-%d %H:%M')
+        response_date = fake.date_time_between(start_date=request_date, end_date=datetime.now().date()).strftime(
+            '%Y-%m-%d %H:%M')
         request_date = request_date.strftime('%Y-%m-%d %H:%M')
         status = random.choice(['PENDING', 'APPROVED', 'REJECTED'])
         club_id = random.randint(1, total_clubs)
         insert_statement += f"""
-INSERT INTO club_membership_request (   request_date, 
-                                        response_date, 
-                                        status, 
-                                        club_id, 
-                                        fighter_id)
-                                        
-    VALUES (    '{request_date}', 
-                '{response_date}', 
-                '{status}', 
-                {club_id}, 
-                {fighter_id});
+INSERT INTO club_membership_request (   request_date, response_date, status, club_id, fighter_id)
+    VALUES ('{request_date}', '{response_date}', '{status}', {club_id}, {fighter_id});
 """
         insert_statement = insert_statement.strip()
         insert_statement += "\n"
@@ -315,19 +276,10 @@ def generate_club_review_insert_statement(total_fighters, total_clubs):
             content = fake.text(max_nb_chars=150)
             rating = random.randint(1, 5)
             review_date = fake.date_time_this_year(before_now=True).strftime('%Y-%m-%d %H:%M')
-            if(random.choice([True, False])):
+            if (random.choice([True, False])):
                 insert_statement += f"""
-INSERT INTO club_review (   content, 
-                            rating, 
-                            review_date, 
-                            club_id, 
-                            fighter_id)
-                            
-    VALUES (    '{content}', 
-                {rating}, 
-                '{review_date}', 
-                {club_id}, 
-                {fighter_id});
+INSERT INTO club_review (content, rating, review_date, club_id, fighter_id)
+    VALUES ('{content}', {rating}, '{review_date}', {club_id}, {fighter_id});
 """
             insert_statement = insert_statement.strip()
             insert_statement += "\n"
@@ -343,19 +295,10 @@ def generate_event_review_insert_statement(total_fighters, total_events):
             content = fake.text(max_nb_chars=150)
             rating = random.randint(1, 5)
             review_date = fake.date_time_this_year(before_now=True).strftime('%Y-%m-%d %H:%M')
-            if(random.choice([True, False])):
+            if (random.choice([True, False])):
                 insert_statement += f"""
-INSERT INTO event_review (  content, 
-                            rating, 
-                            review_date, 
-                            event_id, 
-                            fighter_id)
-                            
-    VALUES (    '{content}', 
-                {rating}, 
-                '{review_date}', 
-                {event_id}, 
-                {fighter_id});
+INSERT INTO event_review (content, rating, review_date, event_id, fighter_id)  
+    VALUES ('{content}', {rating}, '{review_date}', {event_id}, {fighter_id});
 """
             insert_statement = insert_statement.strip()
             insert_statement += "\n"
@@ -368,7 +311,7 @@ def generate_fight_insert_statement(total_events, total_fighters, total_fights):
     insert_statement = "-- FIGHT"
     for event_id in range(1, total_events + 1):
         fight_order = 0
-        for fight in range(1, random.randint(5, total_fights+1)):
+        for fight in range(1, random.randint(5, total_fights + 1)):
             fight_order += 1
             is_title_fight = random.choice([True, False])
             minutes = random.choice([2, 3])
@@ -384,36 +327,15 @@ def generate_fight_insert_statement(total_events, total_fighters, total_fights):
             event_id = random.randint(1, total_events)
             style_id = random.randint(1, 6)
             winner_id = None
-            if(random.choice([True, False])):
+            if (random.choice([True, False])):
                 winner_id = random.choice([blue_corner_fighter_id, red_corner_fighter_id])
 
             insert_statement += f"""
-INSERT INTO fight ( fight_order,
-                    is_title_fight, 
-                    minutes, 
-                    total, 
-                    start_time, 
-                    weight, 
-                    is_ko,
-                    blue_corner_fighter_id, 
-                    category_id, 
-                    event_id, 
-                    red_corner_fighter_id, 
-                    style_id,
+INSERT INTO fight ( fight_order, is_title_fight, minutes, total, start_time, weight, is_ko, 
+                    blue_corner_fighter_id, category_id, event_id, red_corner_fighter_id, style_id,
                     winner_id)
-                    
-    VALUES ({fight_order}, 
-            {is_title_fight}, 
-            {minutes}, 
-            {total}, 
-            '{start_time}', 
-            {weight:.2f}, 
-            {isKo},
-            {blue_corner_fighter_id}, 
-            {category_id}, 
-            {event_id}, 
-            {red_corner_fighter_id}, 
-            {style_id}, 
+    VALUES ({fight_order}, {is_title_fight}, {minutes}, {total}, '{start_time}', {weight:.2f}, {isKo},
+            {blue_corner_fighter_id}, {category_id}, {event_id}, {red_corner_fighter_id}, {style_id}, 
             {f"'{winner_id}'" if winner_id else 'NULL'});
 """
             insert_statement = insert_statement.strip()
@@ -428,40 +350,26 @@ def generate_fight_inscription_request_insert_statement(total_fighters, total_ev
     for fighter_id in range(1, total_fighters + 1):
         club_id = random.randint(1, total_clubs)
         for event_id in range(1, total_events + 1):
-            for fight_id in range(1, 5+1):
+            for fight_id in range(1, 5 + 1):
                 message = fake.text(max_nb_chars=100)
                 request_date = fake.date_time_this_year(before_now=True)
                 status = random.choice(['PENDING', 'APPROVED', 'REJECTED'])
                 response_date = None
                 response = None
                 if status == 'APPROVED' or status == 'REJECTED':
-                    response_date = fake.date_time_between(start_date=request_date, end_date=datetime.now()).strftime('%Y-%m-%d %H:%M')
+                    response_date = fake.date_time_between(start_date=request_date, end_date=datetime.now()).strftime(
+                        '%Y-%m-%d %H:%M')
                     response = fake.text(max_nb_chars=100)
 
                 request_date = request_date.strftime('%Y-%m-%d %H:%M')
                 insert_statement += f"""
-INSERT INTO fight_inscription_request ( message, 
-                                        request_date,
-                                        response,
-                                        response_date,
-                                        status,
-                                        club_id,
-                                        fight_id,
-                                        fighter_id,
-                                        event_id)
-                                        
-    VALUES ('{message}',
-            '{request_date}',
-            {f"'{response}'" if response else 'NULL'},
-            {f"'{response_date}'" if response_date else 'NULL'},
-            '{status}',
-            '{club_id}',
-            '{fight_id}',
-            {fighter_id},
-            {event_id});
+INSERT INTO fight_inscription_request ( message, request_date, response, response_date, status, club_id,
+                                        fight_id, fighter_id, event_id)
+    VALUES ('{message}', '{request_date}', {f"'{response}'" if response else 'NULL'}, 
+            {f"'{response_date}'" if response_date else 'NULL'}, '{status}', '{club_id}',
+            '{fight_id}', {fighter_id}, {event_id});
 """
                 insert_statement = insert_statement.strip()
                 insert_statement += "\n"
 
     return insert_statement
-
--- sql_script_generator.py
+++ sql_script_generator.py
@@ -11,25 +11,29 @@ def generate_sql_script():
     # Add fighter_style data
     sql_script += "\n"+insert_data.generate_fighter_style_insert_statement(10, 6)
     # Add club data
-    sql_script += "\n"+insert_data.generate_club_insert_statement(10, 10)
+    sql_script += "\n"+insert_data.generate_club_insert_statement(5, 10)
     # Associate some fighters with club
-    sql_script += "\n"+insert_data.associate_fighters_with_club(10, 10)
+    sql_script += "\n"+insert_data.associate_fighters_with_club(10, 5)
+    # Add profile photo of fighters
+    sql_script += "\n"+insert_data.generate_fighter_profile_photo_insert_statement(10)
+    # Add club photo
+    sql_script += "\n"+insert_data.generate_club_photo_insert_statement(5)
     # Add follow data
     sql_script += "\n"+insert_data.generate_follow_insert_statement(10)
     # Add fighter_follow_request data
     sql_script += "\n"+insert_data.generate_fighter_follow_request_insert_statement(10)
     # Add event data
-    sql_script += "\n"+insert_data.generate_event_insert_statement(10, 10)
+    sql_script += "\n"+insert_data.generate_event_insert_statement(10, 5)
     # Add club_membership_request data
-    sql_script += "\n"+insert_data.generate_club_membership_request_insert_statement(10, 10)
+    sql_script += "\n"+insert_data.generate_club_membership_request_insert_statement(10, 5)
     # Add club_review data
-    sql_script += "\n"+insert_data.generate_club_review_insert_statement(10, 10)
+    sql_script += "\n"+insert_data.generate_club_review_insert_statement(10, 5)
     # Add event_review data
     sql_script += "\n"+insert_data.generate_event_review_insert_statement(10, 10)
     # Add fight data
     sql_script += "\n"+insert_data.generate_fight_insert_statement(10, 10, 10)
     # Add fight_inscription_request data
-    sql_script += "\n"+insert_data.generate_fight_inscription_request_insert_statement(10, 10, 10)
+    sql_script += "\n"+insert_data.generate_fight_inscription_request_insert_statement(10, 10, 5)
 
     return sql_script
 
